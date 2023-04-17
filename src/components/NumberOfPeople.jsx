b830d28baad34a42bbb763e3e4e7c4bb9d91eee4
Moses <mbtenkorang@yahoo.com>
2023-04-17 22:21:23 +0000

    Improve styles

--- src/components/NumberOfPeople.jsx
+++ src/components/NumberOfPeople.jsx
@@ -2,10 +2,10 @@ import personIcon from '../assets/images/icon-person.svg';
 
 function NumberOfPeople({ numberOfPeople, input_no_of_people }) {
   return (
-    <section className="pb-4 mb-4">
+    <section className="mb-4">
       <label htmlFor="number_of_people">Number Of People</label>
-      <div className="flex items-end">
-        <span className="absolute z-[3] flex items-center pl-4 pt-4">
+      <div className="flex items-center">
+        <span className="absolute z-[3] flex items-center pl-4 pt-2">
           <img
             src={personIcon}
             alt=""
